8c4384fe514af239755209a9c293757f80ea7372
wanghongtao007 <wanhuatong@126.com>
2020-05-20 00:18:15 +0800

    Update curl_customer.sh

--- workshop/content/scripts/curl_customer.sh
+++ workshop/content/scripts/curl_customer.sh
@@ -1,2 +1,2 @@
-export INGRESS_GATEWAY=$(oc get route istio-ingressgateway -n $JUPYTERHUB_USER-smcp -o 'jsonpath={.spec.host}')
+export INGRESS_GATEWAY=$(oc get route istio-ingressgateway -n user1-smcp -o 'jsonpath={.spec.host}')
 while :; do sleep 0.2; curl http://${INGRESS_GATEWAY} ; done
