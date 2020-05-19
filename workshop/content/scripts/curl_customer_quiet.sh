963488bfaa60b633eabdc5f4394afc15c7573dcd
wanghongtao007 <wanhuatong@126.com>
2020-05-20 00:16:50 +0800

    Update curl_customer_quiet.sh

--- workshop/content/scripts/curl_customer_quiet.sh
+++ workshop/content/scripts/curl_customer_quiet.sh
@@ -1,4 +1,4 @@
-export INGRESS_GATEWAY=$(oc get route istio-ingressgateway -n $JUPYTERHUB_USER-smcp -o 'jsonpath={.spec.host}')
+export INGRESS_GATEWAY=$(oc get route istio-ingressgateway -n user1-smcp -o 'jsonpath={.spec.host}')
 
 echo "A load generating script is running in the next step. Ctrl+C to stop"
 
