8cd32204c6d76a9f0456107fe6245b4fa56b80e7
wanghongtao007 <wanhuatong@126.com>
2020-05-22 10:17:44 +0800

    Update curl_recommendation_args.sh

--- workshop/content/scripts/curl_recommendation_args.sh
+++ workshop/content/scripts/curl_recommendation_args.sh
@@ -4,5 +4,5 @@
 # finds the pod IP of the recommendation version
 # curls the found IP with verbose output
 
-export REC_IP=`oc get pod -n ${JUPYTERHUB_USER}-tutorial -l "app=recommendation,version=v2" -o jsonpath='{.items[0].status.podIP}'`
-oc exec -n ${JUPYTERHUB_USER}-tutorial $(oc get pod -n ${JUPYTERHUB_USER}-tutorial -l app=curl -o name | cut -f2 -d/) -- curl -sv $REC_IP:8080
+export REC_IP=`oc get pod -n user1-tutorial -l "app=recommendation,version=v2" -o jsonpath='{.items[0].status.podIP}'`
+oc exec -n user1-tutorial $(oc get pod -n user1-tutorial -l app=curl -o name | cut -f2 -d/) -- curl -sv $REC_IP:8080
