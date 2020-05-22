a623b742c40dcac46b449090f6e8dfe76e370def
wanghongtao007 <wanhuatong@126.com>
2020-05-22 10:16:45 +0800

    Update curl_customer_preference.sh

--- workshop/content/scripts/curl_customer_preference.sh
+++ workshop/content/scripts/curl_customer_preference.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-export CURL_POD=$(oc get pods -n $JUPYTERHUB_USER-tutorial -l app=curl | grep curl | awk '{ print $1}' )
-export CUSTOMER_POD=$(oc get pods -n $JUPYTERHUB_USER-tutorial -l app=customer | grep customer | awk '{ print $1}' )
+export CURL_POD=$(oc get pods -n user1-tutorial -l app=curl | grep curl | awk '{ print $1}' )
+export CUSTOMER_POD=$(oc get pods -n user1-tutorial -l app=customer | grep customer | awk '{ print $1}' )
 
 echo "Curl from outside mesh"
-oc exec -n $JUPYTERHUB_USER-tutorial $CURL_POD -- curl -sv http://preference:8080 
+oc exec -n user1-tutorial $CURL_POD -- curl -sv http://preference:8080 
 echo; echo;
 echo "Curl from inside mesh"
-oc exec -n $JUPYTERHUB_USER-tutorial $CUSTOMER_POD -c customer -- curl -sv http://preference:8080 
+oc exec -n user1-tutorial $CUSTOMER_POD -c customer -- curl -sv http://preference:8080 
 
